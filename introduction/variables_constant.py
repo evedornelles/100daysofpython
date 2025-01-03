ee8ca6b38290398f9a2d8791c9c0625a57f98025
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-03 09:10:00 -0300

    variables and constants

---
+++ introduction/variables_constant.py
@@ -0,0 +1,13 @@
+nome = "Évelim"
+idade = 27
+
+nome = "Maria"
+
+print(nome, idade)
+
+limite_saque_diario = 1000 #snake case
+
+BRAZILIAN_STATES = ["RS", "RJ", "SP"] #constantes
+
+print(BRAZILIAN_STATES)
+
