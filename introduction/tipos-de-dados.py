2b0366c838b63022f9968d35e50dc27f8d18daf6
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-03 08:39:16 -0300

    upload

---
+++ introduction/tipos-de-dados.py
@@ -0,0 +1,13 @@
+             #AULA TIPOS DE DADOS
+
+print (11+10) #Número interino
+print (1.5 + 1 +0.5) #Número ponto flutuante
+print (True) #Tipo booleano
+print (False) #Tipo booleano
+print ("Phyton") #Tipo string
+
+
+int()
+float()
+str()
+bool()
