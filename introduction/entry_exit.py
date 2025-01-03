25a9204119a0308d712a38c99ce0794e98fa985b
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-03 09:35:54 -0300

    entry and exits

---
+++ introduction/entry_exit.py
@@ -0,0 +1,9 @@
+#Funções de entrada e saída
+
+nome = input ("Informe o seu nome: ")
+idade = input("Informe a sua idade: ")
+
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#")
---
+++ introduction/type_conversion.py
@@ -0,0 +1,48 @@
+#Inteiro para float 
+
+preco = 10
+print (preco)
+
+preco = float(preco)
+print(preco)
+
+precp = 10/2
+print(preco)
+
+#Float para inteiro
+
+preco = 10.30
+print(preco)
+
+preco = int(preco)
+print(preco)
+
+#Conversão por divisão
+
+preco = 10
+print(preco)
+
+print(preco / 2)
+
+print(preco //2)
+
+#Numérico para string
+
+preco = 10.50
+idade = 28
+
+print(str(preco))
+
+print (str(idade))
+
+texto = f"idade {idade} preco {preco}"
+print(texto)
+
+#String para número
+
+preco = "10.50"
+idade = 28
+
+print(float(preco))
+
+print(int(idade))
