7f3cb5f1d76840614d3d240d48f102105bb02a71
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-04 08:16:00 -0300

    upload

---
+++ introduction/operators.py
@@ -0,0 +1,13 @@
+#Operadores aritméticos
+
+produto_1 = 10
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
