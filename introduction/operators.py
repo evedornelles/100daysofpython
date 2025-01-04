e9e6de9df033969d14c2d65ca40645f70f90a5d1
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-04 10:00:43 -0300

    operators

--- introduction/operators.py
+++ introduction/operators.py
@@ -1,3 +1,5 @@
+
+
 #Operadores aritméticos
 
 produto_1 = 10
@@ -58,5 +60,36 @@ print(saldo)
 saldo *= 3
 print(saldo)
 
+# Operadores lógicos
+
+#AND = para ser True tudo tem que ser True
+#OR = para ser True apenas um tem que ser True
+
+print(True and True)
+print(True and False)
+print (False and False)
+print(True or True)
+print(True or False)
+print (False or False)
 
 
+saldo = 1000
+saque = 250
+limite = 200
+conta_especial = True
+
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+
+print(exp)
+
+exp_2 = (saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
+
+print(exp_2)
+
+#Operadores de identidade
+
+saldo = 1000
+limite = 1000
+
+print(saldo is limite)
+print (saldo is not limite)
