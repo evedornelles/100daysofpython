d32890db1062d3b08575069b59c70186584aa701
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-04 08:44:20 -0300

    upload

--- introduction/operators.py
+++ introduction/operators.py
@@ -11,3 +11,18 @@ print(produto_1 * produto_2)
 print(produto_1 % produto_2)
 print(produto_1 ** produto_2)
 
+x = 10 + 5 * 4
+print(x)
+
+# Operadores de comparação
+
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
+
