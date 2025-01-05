1eb20aee84876158601747e1ad51c98b8e35bd02
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-05 08:56:06 -0300

    conditional and repetition structures

--- introduction/conditional_repetitionstructures.py
+++ introduction/conditional_repetitionstructures.py
@@ -32,3 +32,26 @@ elif idade == IDADE_ESPECIAL:
     print("Pode fazer aulas teóricas, mas não pode fazer aulas práticas")
 else:
     print("Ainda não pode tirar a CNH")
+
+#Estrutura condicional alinhada
+
+conta_normal = True
+conta_universitaria = False
+
+saldo = 2000
+saque = 500
+cheque_especial = 450
+
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com uso do cheque especial")
+    else:
+        print("Não foi possível realizar o saque!")
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo insuficiente")
+
