8bcaab6532413ee9061acaf1b3575a1ac1711748
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-06 11:21:50 -0300

    structures

--- introduction/strings.py
+++ introduction/strings.py
@@ -42,3 +42,10 @@
   linguagem = "Python"
   
   print("Nome: %s Idade %d" % (nome, idade))
+  print("Nome: {} Idade: {}".format(nome, idade))
+  print("Nome: {1} Idade: {0}".format(nome, idade))
+  print("Nome: {1} Idade: {0} Nome: {1} {1}".format(nome, idade))
+  print("Nome: {nome} Idade: {idade}".format(nome=nome, idade=idade))
+  print("Nome: {name} Idade: {age}".format(name=nome, age=idade))
+
+
