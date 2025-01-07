8f13507be8247913561df582f214e41ded4175cd
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-07 11:22:53 -0300

    upload

--- Structures/lists.py
+++ Structures/lists.py
@@ -34,3 +34,47 @@ print(matriz[0])  # [1, "a", 2]
 print(matriz[0][0])  # 1
 print(matriz[0][-1])  # 2
 print(matriz[-1][-1])  # "c"
+
+#Fatiamento
+
+lista = ["p", "y", "t", "h", "o", "n"]
+
+print(lista[2:])  # ["t", "h", "o", "n"]
+print(lista[:2])  # ["p", "y"]
+print(lista[1:3])  # ["y", "t"]
+print(lista[0:3:2])  # ["p", "t"]
+print(lista[::])  # ["p", "y", "t", "h", "o", "n"]
+print(lista[::-1])  # ["n", "o", "h", "t", "y", "p"]
+
+#Iterar listas
+
+carros = ["gol", "celta", "palio"]
+
+for carro in carros:
+    print(carro)
+
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+#Compreensão de listas
+
+# Filtrar lista
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Modificar valores
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
+
+#Appendy
+
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)  # [1, "Python", [40, 30, 20]]
