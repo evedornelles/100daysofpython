2bfe255aca52eaada2501c02dbeb6bb7974895e9
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-11 15:49:00 -0300

    desafio bike

--- 03-POO/desafio_bike.py
+++ 03-POO/desafio_bike.py
@@ -1,9 +1,26 @@
 class Bicicleta:
-    def__init__(self, cor, modelo, ano, valor):
+    def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+    
+    def buzinar(self):
+        print("Plim plim...")
+    
+    def parar(self):
+        print("Parando bicicleta...")
+        print("Bicicleta parada.")
+    
+    def correr(self):
+        print("Vrummm...")
 
 
 
+b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+b1.buzinar()
+b1.correr()
+b1.parar()
+print(b1.cor, b1.modelo, b1.ano, b1.modelo)
+
+b2 = Bicicleta("verde", "monark", "2000", 189)
