b09cdc5c09ff527a7a2a517dc7bf381e453ff065
Évelim Dornelles <dorrrnelles@gmail.com>
2025-01-15 10:29:21 -0300

    encapsulation example

--- 03-POO/04-encapsulation.py
+++ 03-POO/04-encapsulation.py
@@ -46,3 +46,18 @@ del foo.x
 print(foo.x)
 foo.x = 10
 print(foo.x)
+
+#Propriedades pessoa exemplo
+class Pessoa:
+    def __init__(self, nome, ano_nascimento):
+        self.nome = nome
+        self._ano_nascimento = ano_nascimento
+
+    @property
+    def idade(self):
+        _ano_atual = 2022
+        return _ano_atual - self._ano_nascimento
+
+
+pessoa = Pessoa("Guilherme", 1994)
+print(f"Nome: {pessoa.nome} \tIdade: {pessoa.idade}")
